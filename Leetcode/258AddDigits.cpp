e0df8deb4956a303e285f6dd0463ee2272a1a797
Naman Pandey <109365773+Namanlko@users.noreply.github.com>
2024-12-06 18:21:54 +0530

    Initial Commit

---
+++ Leetcode/258AddDigits.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin>>n;
+
+    while(n>9){
+        int ans=0;
+        while(n>0){
+            int digit = n%10;
+            ans = ans + digit;
+            n = n/10;
+        }
+        n = ans;
+    }
+
+    cout<<"Result is "<<n;
+
+    return 0;
+}
